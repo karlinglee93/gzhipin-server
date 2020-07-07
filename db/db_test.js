2e1ffd87fe749b18b71557d66710cf5b3fd9d3c7
lijianig <lijianig@cn.ibm.com>
2020-07-07 16:43:04 +0800

    feat(Mongoose): create and connect db

---
+++ db/db_test.js
@@ -0,0 +1,39 @@
+/**
+ * 测试使用mongoose操作mongodb
+ * 1. 连接数据库
+ * 	1.1. 引入 mongoose
+ * 	1.2. 连接指定数据库(URL 只有数据库是变化的) 
+ * 	1.3. 获取连接对象
+ * 	1.4. 绑定连接完成的监听(用来提示连接成功)
+ * 2. 得到对应特定集合的 Model
+ * 	2.1. 字义 Schema(描述文档结构)
+ * 	2.2. 定义 Model(与集合对应, 可以操作集合)
+ * 3. 通过 Model 或其实例对集合数据进行 CRUD 操作
+ * 	3.1. 通过 Model 实例的 save()添加数据
+ * 	3.2. 通过 Model 的 find()/findOne()查询多个或一个数据 
+ * 	3.3. 通过 Model 的 findByIdAndUpdate()更新某个数据 
+ * 	3.4. 通过 Model 的 remove()删除匹配的数据
+ */
+/* 1. 连接数据库 */
+// 1.1. 引入 mongoose
+const mongoose = require('mongoose')
+// 1.2. 连接指定数据库(URL 只有数据库是变化的) 
+mongoose.connect('mongodb://localhost:27017/gzhipin_test')
+// 1.3. 获取连接对象
+const conn = mongoose.connection
+// 1.4. 绑定连接完成的监听(用来提示连接成功)
+conn.once('open', () => {
+	console.log('gzhipin_test 数据库连接成功！')
+})
+
+/* 2. 得到对应特定集合的 Model */
+//	2.1. 字义 Schema(描述文档结构)
+const userSchema = mongoose.Schema({
+	// type, required, default
+	username: {type: String, required: true},
+	password: {type: String, required: true},
+	type: {type: String, required: true}
+})
+//	2.2. 定义 Model(与集合对应, 可以操作集合)
+// 其值为构造函数
+const UserModel = mongoose.model('user', userSchema)
