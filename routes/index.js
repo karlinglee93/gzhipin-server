2949461e72bbd6dc508408124eee70f998b35b28
lijianig <lijianig@cn.ibm.com>
2020-07-08 00:02:56 +0800

    feat(Login): create route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const md5 = require('blueimp-md5')
 const {UserModel} = require('../db/models')
+const filter = {password: 0, __v: 0}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -35,7 +36,7 @@ router.post('/register', (req, res) => {
         new UserModel({username, password: md5(password), type}).save((err, user) => {
           if (!err) {
             // 生成一个cookie(user_id: user._id), 并交给浏览器保存
-            // 此方法经前端处理后可以让用户1天内免登录
+            // 持久化cookie, 此方法经前端处理后, 浏览器会保存在本地文件，可以让用户1天内免登录
             res.cookie('user_id', user._id, {maxAge: 1000 * 60 * 60 * 24})
             res.send({code: 0, data: {_id: user._id, username, type}})
           } else {
@@ -44,6 +45,19 @@ router.post('/register', (req, res) => {
         })
       }
     })
+})
+
+router.post('/login', (req, res) => {
+  const {username, password} = req.body
+  UserModel.findOne({username, password: md5(password)}, filter, (err, user) => {
+    if (user) {
+      // 持久化cookie
+      res.cookie('user_id', user._id, {maxAge: 1000 * 60 * 60 * 24})
+      res.send({code: 0, data: user})
+    } else {
+      res.send({code: 1, msg: '用户名或密码错误!'})
+    }
+  })
 })
 
 module.exports = router;
