cb32531f4bd92a02b067841220ad2d6c10c3a883
karlinglee93 <459607025@qq.com>
2020-08-12 00:03:59 +0800

    fit(sendMsg): update

--- socketIO/server_socket.js
+++ socketIO/server_socket.js
@@ -1,12 +1,21 @@
+const {ChatModel} = require('../db/models')
+
 module.exports = (server) => {
     const io = require('socket.io')(server)
 
     io.on('connection', socket => {
         console.log('socket connected')
-        socket.on('receiveMsg', ({from, to, data}) => {
-            console.log('客户端收到消息: ' + {from, to, data})
-            socket.emit('sendMsg', {from, to, data})
-            console.log('客户端发送消息: ' + {from, to, data})
+        socket.on('sendMsg', ({from, to, content}) => {
+            console.log('服务端接收消息: ', {from, to, content})
+            // 处理数据(保存消息)
+            // 准备chatMsg对象的相关数据
+            const chat_id = [from, to].sort().join('_')
+            const create_time = Date.now()
+            new ChatModel({chat_id, from, to, content, create_time}).save((err, chatMsg) => {
+                // 向客户端发送消息(发给所有人, 有待优化)
+                io.emit('receiveMsg', chatMsg)
+                console.log('服务端发送消息: ', chatMsg)
+            })
         })
     })
 }
